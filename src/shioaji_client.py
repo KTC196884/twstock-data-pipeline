07dc2a2d27287371e9547a9b6bc8a0ada9c45f69
Aaron 陳冠廷 <58315633+aaron196884@users.noreply.github.com>
2025-01-03 18:07:45 +0800

    Update shioaji_client.py

--- src/shioaji_client.py
+++ src/shioaji_client.py
@@ -1,9 +1,24 @@
 import shioaji as sj
+import logging
+
+from config import REMAINING_BYTES_THRESHOLD
 
 def login_shioaji(api_key, secret_key):
-    """
+    '''
     建立並登入 Shioaji 連線
-    """
+    '''
     api = sj.Shioaji(simulation=True)
     api.login(api_key=api_key, secret_key=secret_key)
+    logging.info('API successfully accessed')
     return api
+
+def check_api_limit(api) -> bool:
+    '''
+    檢查 API 用量
+    '''
+    remaining = api.usage().remaining_bytes
+    logging.info(f'API Remaining bytes: {remaining}')
+    if remaining < REMAINING_BYTES_THRESHOLD:
+        logging.warning('API usage limit reached')
+        return False
+    return True
