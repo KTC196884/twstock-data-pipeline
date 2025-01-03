3dfaa2628fdcbca41d9bedfbe9f9f63842df201e
Aaron 陳冠廷 <58315633+aaron196884@users.noreply.github.com>
2025-01-03 18:06:33 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,18 +2,18 @@ from pathlib import Path
 from datetime import date
 
 # === 路徑及常數設定 ===
-DATA_FOLDER = Path("/Users/jack/Documents/Quant/basic_data/TW_stock_1mk")
-TWSTK_INFO_PATH = Path('THE_PATH_OF_TWstk_info.parquet')
-TRADINGDAY_LIST_PATH = Path('/Users/jack/Documents/Quant/basic_data/tradingday_list.npy')
+TWSTK_INFO_PATH = Path('THE_PATH_OF_twstk_info.parquet')
+TRADINGDAY_LIST_PATH = Path('THE_PATH_OF_tradingday_list.npy')
 
-# START_DATE = '2018-12-07'
-START_DATE = '2024-12-30'
+# START_DATE = '2018-12-07' # shioaji api 1mk 資料起始日
+START_DATE = '2024-12-15'
 END_DATE = date.today().strftime("%Y-%m-%d")
 
 REMAINING_BYTES_THRESHOLD = 1e6
 SLEEP_TIME = 0.25
 
-STOCK_IDX_START, STOCK_IDX_END = (1268, 2170)
+# STOCK_IDX_START, STOCK_IDX_END = (0, 2170)
+STOCK_IDX_START, STOCK_IDX_END = (0, 2170)
 
 # === API KEY ===
 API_KEY = 'YOUR_API_KEY'
