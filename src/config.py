5b2a3846261b3cdb4f928a0ca22c98fcb3b80c71
Kuan-Ting (Jack) Chen <58315633+KTC196884@users.noreply.github.com>
2025-03-29 23:27:06 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from datetime import date
+from datetime import date, timedelta
 
 # === 路徑及常數設定 ===
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -12,21 +12,20 @@ LOGS_DIR = PROJECT_ROOT / "logs"
 TWSTK_INFO_PATH = DATA_DIR / "twstk_info.parquet"
 TRADING_DAYS_PATH = DATA_DIR / "trading_days.parquet"
 TWSTK_1mk_PATH = DATA_DIR / "twstk_1mk"
+TWSTK_PROGRESS_PATH = DATA_DIR / "twstk_progress.pkl"
 
 # 日誌檔位置
 LOG_FILE_PATH = LOGS_DIR / "twstk_1mk_fetching.log"
 
 #
-START_DATE = '2025-02-01' # START_DATE must >= '2018-12-07'
+START_DATE = '2018-12-07' # START_DATE must >= '2018-12-07'
 END_DATE = date.today().strftime("%Y-%m-%d")
+# END_DATE = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 #
 REMAINING_BYTES_THRESHOLD = 1e6
 SLEEP_TIME = 0.25
 
-#
-STOCK_IDX_START, STOCK_IDX_END = (0, 10)
-
 # === API KEY ===
-API_KEY = 'YOUR_API_KEY'
-SECRET_KEY = 'YOUR_SECRET_KEY'
+API_KEY = 'YOUR API KEY'
+SECRET_KEY = 'YOUR SECRET KEY'
